04c32554130717dd4cfcd350cbc7fb3a2d9f84e2
Gulzhanat Zhumazhanova <zhumazhanova.gulzhanat01@gmail.com>
2022-12-24 22:38:32 +0500

    Task 977 Squares of a sorted array

---
+++ LeetCode/LeetCode.UnitTests/SquaresOfSortedArrayTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.UnitTests
+{
+    public class SquaresOfSortedArrayTests
+    {
+        [TestCase(new int[] { -4, -1, 0, 3, 10 }, new int[] { 0, 1, 9, 16, 100 })]
+        [TestCase(new int[] { -7, -3, 2, 3, 11 }, new int[] { 4, 9, 9, 49, 121 })]
+        public void SortedSquares_ValidArray_ReturnsCorrectResult(int[] nums, int[] expectedResult)
+        {
+            var actualResult = SquaresOfSortedArray.SortedSquares(nums);
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ LeetCode/LeetCode/SquaresOfSortedArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class SquaresOfSortedArray
+    {
+        public static int[] SortedSquares(int[] nums)
+        {
+            var indexOfZero=SearchInsertPosition.SearchInsert(nums, 0);
+
+            for (int i = 0; i < indexOfZero; i++)
+            {
+                nums[i] = Math.Abs(nums[i]);
+            }
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] *= nums[i];
+            }
+
+            return nums;
+        }
+    }
+}
