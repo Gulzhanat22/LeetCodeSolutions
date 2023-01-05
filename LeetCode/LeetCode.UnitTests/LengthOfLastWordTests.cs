7ab08cec5e8b80b24e62b7929aa513e79c3a65f6
Gulzhanat Zhumazhanova <zhumazhanova.gulzhanat01@gmail.com>
2023-01-05 22:33:18 +0500

    Task 58 Length of Last Word

---
+++ LeetCode/LeetCode.UnitTests/LengthOfLastWordTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.UnitTests
+{
+    public class LengthOfLastWordTests
+    {
+        [TestCase("Hello World", 5)]
+        [TestCase("   fly me   to   the moon  ", 4)]
+        public void FindLengthOfLastWord_ValidString_ReturnCorrectResult(string s, int expectedResult)
+        {
+            var actualResult = LengthOfLastWord.FindLengthOfLastWord(s);
+
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+        }
+    }
+}
---
+++ LeetCode/LeetCode/LengthOfLastWord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class LengthOfLastWord
+    {
+        public static int FindLengthOfLastWord(string s)
+        {
+            var words=s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var length = words[words.Length-1].Length;
+            return length;
+        }
+    }
+}
